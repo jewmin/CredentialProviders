e3e704be15e500ebf79c5c23cdab226fd62aeecf
jewmin <jewmin@gmail.com>
2021-01-29 19:31:23 +0800

    修复显示问题

--- Gina/UI/NoticeDialog.cpp
+++ Gina/UI/NoticeDialog.cpp
@@ -4,13 +4,22 @@
 NoticeDialog::NoticeDialog(IWinlogon * pWinlogon, HANDLE hUserToken, int nDialogResourceID)
     : GinaModalDialog(pWinlogon, nDialogResourceID)
     , hUserToken_(hUserToken) {
-    if (nDialogResourceID == IDD_LOCKEDNOTICE) {
-        WCHAR szUserName[GinaModalDialog::MaxSizeUserName] = L"";
-        if (SecurityHelper::ImpersonateAndGetUserName(hUserToken_, szUserName, GinaModalDialog::MaxSizeUserName)) {
-            GuiHelper::SetControlText(hWnd_, IDC_NAME, Utils::StringFormat(L"Only %s or an administrator", szUserName).c_str());
-        }
-    }
 }
 
 NoticeDialog::~NoticeDialog() {
+}
+
+INT_PTR NoticeDialog::DialogProc(UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    switch (uMsg) {
+    case WM_INITDIALOG: {
+            if (nDialogResourceID_ == IDD_LOCKEDNOTICE) {
+                WCHAR szUserName[GinaModalDialog::MaxSizeUserName] = L"";
+                if (SecurityHelper::ImpersonateAndGetUserName(hUserToken_, szUserName, GinaModalDialog::MaxSizeUserName)) {
+                    GuiHelper::SetControlText(hWnd_, IDC_NAME, Utils::StringFormat(L"Only %s or an administrator", szUserName).c_str());
+                }
+            }
+        }
+        return TRUE;
+    }
+    return FALSE;
 }
--- Gina/UI/NoticeDialog.h
+++ Gina/UI/NoticeDialog.h
@@ -6,6 +6,7 @@ class NoticeDialog : public GinaModalDialog {
 public:
     NoticeDialog(IWinlogon * pWinlogon, HANDLE hUserToken, int nDialogResourceID);
     virtual ~NoticeDialog();
+    virtual INT_PTR DialogProc(UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 private:
     HANDLE hUserToken_;
