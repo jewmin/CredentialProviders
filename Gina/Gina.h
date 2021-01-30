28a07643dbbe22f7338506fba14214a184efc51a
jim chen <chenzemingczm@163.com>
2021-01-30 11:08:11 +0800

    添加注释

--- Gina/Gina.h
+++ Gina/Gina.h
@@ -7,22 +7,30 @@
 class Gina {
 public:
     static BOOL Negotiate(DWORD dwWinlogonVersion, PDWORD pdwDllVersion);
+    // 初始化
     static BOOL Initialize(HANDLE hWlx, PVOID pWinlogonFunctions, Gina * * ppGina);
 
 public:
+    // 未登录
     int LoggedOutSAS(DWORD dwSasType, PLUID pAuthenticationId, PSID pLogonSid, PDWORD pdwOptions, PHANDLE phToken, PWLX_MPR_NOTIFY_INFO pNprNotifyInfo, PVOID * ppWinlogonProfile);
+    // 已登录
     int LoggedOnSAS(DWORD dwSasType);
+    // 锁定
     int WkstaLockedSAS(DWORD dwSasType);
 
     BOOL ActivateUserShell(PWSTR pszDesktopName, PWSTR pszMprLogonScript, PVOID pEnvironment);
 
+    // 未登录时显示ctrl+del+alt对话框
     VOID DisplaySASNotice();
+    // 锁定时显示ctrl+del+alt对话框
     VOID DisplayLockedNotice();
 
     BOOL IsLockOk();
     BOOL IsLogoffOk();
 
+    // 注销
     VOID Logoff();
+    // 关机
     VOID Shutdown(DWORD ShutdownType);
 
     BOOL ScreenSaverNotify(BOOL * pSecure);
@@ -30,6 +38,7 @@ public:
 
     BOOL NetworkProviderLoad(PWLX_MPR_NOTIFY_INFO pNprNotifyInfo);
 
+    // 显示windows状态消息
     BOOL DisplayStatusMessage(HDESK hDesktop, DWORD dwOptions, PWSTR pTitle, PWSTR pMessage);
     BOOL GetStatusMessage(DWORD * pdwOptions, PWSTR pMessage, DWORD dwBufferSize);
     BOOL RemoveStatusMessage();
